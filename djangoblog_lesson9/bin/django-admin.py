188d7cba888ada1fb9cd75a230b7523fd443336d
seaneightysix <sean.eightysix@GMAIL.com>
2019-08-02 15:07:49 -0500

    updated requirements.txt

---
+++ djangoblog_lesson9/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/seantasaki/Desktop/Python3/djangoblog_lesson9/djangoblog_lesson9/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
